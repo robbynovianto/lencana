eca153ee17a13e28e4e4c5ba888287a650220cbe
Robby Novianto <robbyhahaha@gmail.com>
2022-01-09 04:30:23 +0700

    proses membuat footer

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -110,6 +110,8 @@
 
     </div>
 
+
+
     <script>
         @if(session()->has('success'))
             toastr.success('{{ session('success') }}')
@@ -160,4 +162,59 @@
 
     </script>
 </body>
+
+<footer>
+    <div class="main-content">
+        <div class="left box">
+            <h2>About us</h2>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis autem enim voluptates nesciunt error quae nobis, 
+                    culpa, neque expedita, quisquam sequi. Ut delectus, quasi similique assumenda minus voluptatum eaque consectetur.</p>
+            <div class="social">    
+                <a href="#"><span class="fab fa-facebook-f"></span></a>
+                <a href="#"><span class="fab fa-twitter"></span></a>
+                <a href="#"><span class="fab fa-instagram"></span></a>
+            </div>
+            </div>  
+        </div>
+        <div class="center box">
+            <h2>Address</h2>
+            <div class="content">
+                <div class="place">
+                    <span class="fas fa-map-marker-alt"></span>
+                    <span class="text">Ds. Ledok Demaan RT3/5 no 885, Kudus</span>
+                </div>
+                <div class="phone">
+                    <span class="fas fa-phone-alt"></span>
+                    <span class="text">+6287730799869</span>
+                </div>
+                <div class="email">
+                    <span class="fas fa-envelope"></span>
+                    <span class="text">abc@example.com</span>
+                </div>
+            </div>
+        </div>
+        <div class="right box">
+            <h2>Contact us</h2>
+            <div class="content">
+                <form action="#">
+                    <div class="email">
+                        <div class="text">Email *</div>
+                        <input type="email" required>
+                    </div>
+                    <div class="msg">
+                        <div class="text">Message *</div>
+                        <textarea cols="25" rows="2" required></textarea>
+                    </div>
+                    <div class="btn">
+                        <button type="submit">Send</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+</div>
+
+    <div class="center box"></div>
+</footer>
 </html>
